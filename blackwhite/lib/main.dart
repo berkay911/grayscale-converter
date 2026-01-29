bb6b7f24635f27ebbb544dc434ae75d3753d9a9e
Berkay <berkay9114@proton.me>
2026-01-29 19:50:21 +0300

    better result page

--- blackwhite/lib/main.dart
+++ blackwhite/lib/main.dart
@@ -134,16 +134,33 @@ class GrayscalePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Positioned.fill(
-            child: Image.memory(grayscaleImage, fit: BoxFit.cover),
+      appBar: AppBar(
+        title: Text(
+          "Grayscale Converter",
+          style: TextStyle(
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
           ),
-          Positioned(
-            bottom: 40,
-            left: 20,
-            right: 20,
-            child: ElevatedButton(
+        ),
+        centerTitle: true,
+        backgroundColor: Theme.of(context).appBarTheme.backgroundColor,
+        elevation: 0,
+        iconTheme: IconThemeData(color: Colors.black),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 70),
+            Container(
+              width: 370,
+              height: 370,
+              color: Colors.grey[300],
+              child: Image.memory(grayscaleImage, fit: BoxFit.cover),
+            ),
+            SizedBox(height: 40),
+            ElevatedButton(
+              child: Text("Download", style: TextStyle(fontSize: 16)),
               onPressed: () async {
                 Directory? directory;
                 if (Platform.isAndroid) {
@@ -158,22 +175,17 @@ class GrayscalePage extends StatelessWidget {
                   SnackBar(content: Text('Image saved to $filePath')),
                 );
               },
-              child: Text("Download", style: TextStyle(fontSize: 16)),
               style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all<Color>(
-                  const Color.fromARGB(255, 1, 4, 99),
-                ),
-                foregroundColor: MaterialStateProperty.all<Color>(
-                  const Color.fromARGB(255, 255, 255, 255),
-                ),
+                backgroundColor: MaterialStateProperty.all<Color>(Colors.black),
+                foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
                 minimumSize: MaterialStateProperty.all<Size>(Size(370, 50)),
                 shape: MaterialStateProperty.all<RoundedRectangleBorder>(
                   RoundedRectangleBorder(borderRadius: BorderRadius.zero),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
